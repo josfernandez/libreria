46524b2c001631c90cffaa145c87e77aaae20678
josfernandez <103599049+josfernandez@users.noreply.github.com>
2025-07-25 02:10:34 -0300

    Update libreria.pl

--- libreria.pl
+++ libreria.pl
@@ -1,1 +1,23 @@
 % Predicados
+%vende(Titulo,Autor,Genero,Editorial,Precio)
+%vende(Articulo,Precio)
+vende(libro(elResplandor, stephenKing, terror,debolsillo),          2300).
+vende(libro(cronicasdeangeles, alejandroDolina, ficcion,booket),    1600).
+vende(libro(harryPotter, jkRowling, ficcion, salamandra),           2500).
+vende(cd(elamordespuesdelamor, fito, rock, 10 , 15),                 450).
+vende(cd(pubisangelical, charly, rock, 10, 15),                      500).
+
+%tematico(Autor): se cumple para un autor si todo lo que se vende es de el
+
+tematico(Autor):- 
+    autor(_,Autor),
+    forall(vende(Articulo,_), autor(Articulo, Autor)).
+
+autor(libro(_,Autor,_,_), Autor):-vende(libro(_,Autor,_,_),_).
+autor(cd(_,Autor,_,_,_), Autor):-vende(cd(_,Autor,_,_,_),_).
+
+
+%libromascaro/1 se cumple para un articulo si es el libro de mayor precio
+
+libromascaro(libro(Titulo,Autor,Genero,Editorial)):-
+    forall(vende(libro(_,_,_,_),Precio1),vende(libro(_,_,_,_),Precio1)<Precio).

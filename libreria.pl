9d86520553ee6d2c336f204203ab9dbe8d5e915f
Jose Luis Fernandez <josfernandez@frba.utn.edu.ar>
2025-07-25 00:44:32 -0300

    segundo commit

--- libreria.pl
+++ libreria.pl
@@ -1,1 +1,19 @@
 % Predicados
+%vende(Titulo,Autor,Genero,Editorial,Precio)
+%vende(Articulo,Precio)
+vende(libro(elResplandor, stephenKing, terror,debolsillo),          2300).
+vende(libro(cronicasdeangeles, alejandroDolina, ficcion,booket),    1600).
+vende(libro(harryPotter, jkRowling, ficcion, salamandra),           2500).
+vende(cd(elamordespuesdelamor, fito, rock, 10 , 15),                 450).
+vende(cd(pubisangelical, charly, rock, 10, 15),                      500).
+
+%tematico(Autor): se cumple para un autor si todo lo que se vende es de el
+
+tematico(Autor):- 
+    autor(_,Autor),
+    forall(vende(Articulo,_), autor(Articulo, Autor)).
+
+autor(libro(_,Autor,_,_), Autor):-vende(libro(_,Autor,_,_),_).
+autor(cd(_,Autor,_,_,_), Autor):-vende(cd(_,Autor,_,_,_),_).
+
+
